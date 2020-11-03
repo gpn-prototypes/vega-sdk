c5af1a953c2e21f009e597c5287dec3763a1f2bc
Dmitriy Klimenko <33551076+hitmanet@users.noreply.github.com>
2020-11-03 15:17:51 +0300

    Настройка публикации SDK (#1)
    * v1.0.1
    
    * feat(publish): настройка публикации
    
    index 68364b5..dfdb7de 100644
    --- a/.npmrc
    +++ b/.npmrc
    @@ -1,2 +1,2 @@
    
    @gpn-prototypes:registry=https://npm.pkg.github.com
    -registry=https://npm.pkg.github.com
    +registry=https://npm.pkg.github.com/gpn-prototypes
    diff
    --git a/package.json b/package.json
    index 720997a..97e26a2 100644
    --- a/package.json
    +++
    b/package.json
    @@ -1,7 +1,6 @@
    {
      "name": "vega-sdk",
      "version": "1.0.1",
    -  "repository":
    "git@github.com:gpn-prototypes/vega-sdk.git",
      "author": "gpn-prototypes",
      "main":
    "dist/index.js",
      "types": "dist/index.d.ts",
    @@ -18,6 +17,14 @@
          "config":
    "./node_modules/@gpn-prototypes/frontend-configs/git/commitizen.js"
        }
      },
    +  "publishConfig":
    {
    +    "access": "public",
    +    "registry": "https://npm.pkg.github.com/gpn-prototypes"
    +  },
    +
    "repository": {
    +    "type": "git",
    +    "url": "ssh://git@github.com:gpn-prototypes/vega-sdk.git"
    +
    },
      "scripts": {
        "build": "tsc --build tsconfig.json",
        "test": "jest",
    
    * feat(publish): добавление паблиша
    
    * feat(github): добавил CODEOWNERS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,5 @@
 module.exports = {
   extends: [require.resolve('@gpn-prototypes/frontend-configs/.eslintrc')],
-  overrides: [
-    {
-      files: ['./src/**/index.stories.tsx'],
-      rules: {
-        'import/no-default-export': ['off'],
-      },
-    },
-  ],
   rules: {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'error',
