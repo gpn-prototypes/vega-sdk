d387e9207ed25c09e00a7083269e8c1ee97aa94a
hitmanet <nathan200033@gmail.com>
2020-11-30 11:22:56 +0300

    feat(packages): обновил утилиты

--- src/identity/identity.ts
+++ src/identity/identity.ts
@@ -4,9 +4,12 @@ export type IdentityConfigType = {
   apiUrl: string;
   token?: string;
   cbOnAuth?(): void;
+  cbOnLogout?(): void;
 };
 
 const AUTH_TOKEN = 'auth-token';
+
+const noop = () => {};
 
 export class Identity {
   private apiClient: APIClient;
@@ -15,38 +18,52 @@ export class Identity {
 
   private cbOnAuth?(): void;
 
+  private cbOnLogout?(): void;
+
   constructor(config: IdentityConfigType) {
-    const { apiUrl, token = null, cbOnAuth = () => {} } = config;
+    const { apiUrl, token = null, cbOnAuth = noop, cbOnLogout = noop } = config;
     this.apiClient = new APIClient(apiUrl);
     this.cbOnAuth = cbOnAuth;
+    this.cbOnLogout = cbOnLogout;
     if (token) {
       localStorage.setItem(this.AUTH_TOKEN, token);
     }
   }
 
+  // eslint-disable-next-line consistent-return
   public auth = async ({ login, password }: UserDataType): Promise<void> => {
-    const { token } = await this.apiClient.auth({ login, password });
-    if (token) {
-      if (typeof this.cbOnAuth === 'function') {
-        this.cbOnAuth();
-      }
+    try {
+      const { token } = await this.apiClient.auth({ login, password });
+      if (token) {
+        if (typeof this.cbOnAuth === 'function') {
+          this.cbOnAuth();
+        }
 
-      localStorage.setItem(this.AUTH_TOKEN, token);
+        localStorage.setItem(this.AUTH_TOKEN, token);
+      }
+    } catch (err) {
+      return Promise.reject(err);
     }
   };
 
-  public getToken = (): IdentityConfigType['token'] | null => {
+  public getToken = (): string | null => {
     const authToken = localStorage.getItem('auth-token');
     return authToken || null;
   };
 
-  public updateToken = (newToken: IdentityConfigType['token']): void => {
-    if (newToken) {
-      localStorage.setItem(this.AUTH_TOKEN, newToken);
-    }
+  public isLoggedIn(): boolean {
+    const token = this.getToken();
+    return token !== null;
+  }
+
+  public updateToken = (newToken: string): void => {
+    localStorage.setItem(this.AUTH_TOKEN, newToken);
   };
 
   public logout = (): void => {
+    if (typeof this.cbOnLogout === 'function') {
+      this.cbOnLogout();
+    }
     localStorage.removeItem(this.AUTH_TOKEN);
   };
 }
