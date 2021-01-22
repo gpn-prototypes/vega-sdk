5569345e9e448203d3d112ec647ef847b4b5f649
Maksim Kononov <maksim.kononov.csssr@gmail.com>
2021-01-22 10:43:26 +0300

    feat(global): альфа-версия
    Jira issue: VEGA-857

---
+++ configs/jest/jest.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = ({ setupFilesAfterEnv }) => ({
+  verbose: true,
+  setupFilesAfterEnv: [setupFilesAfterEnv],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.join(
+      __dirname,
+      './mocks/file-mock.js',
+    ),
+    '\\.(css|less)$': path.join(__dirname, './mocks/style-mock.js'),
+  },
+  testMatch: ['**/*.test.{ts,tsx}'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/types/'],
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.stories.tsx'],
+});
